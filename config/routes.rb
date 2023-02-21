94145fb78c30a28e45ce282530578a9e2b1d495e
YOU PAN <sunnypanyou@gmail.com>
2023-02-21 15:24:37 -0500

    the 1st version without CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+
 end
